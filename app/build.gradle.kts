c87b37d2e9ae256ebddeba072ebc273cf7531f2b
kingzcheung <kingzcheung@gmail.com>
2026-04-13 17:54:23 +0800

    chore: bump version to 1.3.2
    - Update versionCode: 5 -> 6
    - Update versionName: 1.3.1 -> 1.3.2
    - Includes ONNX model download fixes and test workflow improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.kingzcheung.kime"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.1"
+        versionCode = 6
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
